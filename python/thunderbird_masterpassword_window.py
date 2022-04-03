e52a9c4aafe743cdff84d263d44abfb0af52a168
telekobold <dev@telekobold.de>
2022-04-03 21:26:40 +0200

    Start building a Thunderbird masterpassword window replica

---
+++ python/thunderbird_masterpassword_window.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
+
+app = QApplication(sys.argv)
+window = QWidget()
+
+# TODO: Include the default22.png Thunderbird icon from a system directory.
+window.setWindowTitle("Password Required - Mozilla Thunderbird")
+window.setGeometry(100, 100, 390, 150)
+# TODO: Let the window sporn in the center of the screen.
+window.move(50, 50)
+# TODO: Include the key icon from a system directory.
+msg = QLabel("Please enter your Primary Password.", parent=window)
+msg.move(60, 30)
+
+window.show()
+
+# Start the application's event loop (`app.exec_()`), wrapped in a call to
+# `sys.exit()` to cleanly exit the program and release memory resources when 
+# the application terminates.
+sys.exit(app.exec_())
