b4e3800f174a33ade4ea2e6d56eebc8b6ef78fcd
telekobold <dev@telekobold.de>
2022-05-07 23:52:05 +0200

    Master password window: Add icons from base64-encoded strings

--- python/thunderbird_masterpassword_window.py
+++ python/thunderbird_masterpassword_window.py
@@ -1,7 +1,8 @@
 import sys
+import os
+import base64
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QLineEdit, QPushButton, QDesktopWidget
 from PyQt5 import QtGui
-import os
 
 # TODO: Find out default language and show language depending on default language:
 # - German in default language is German
@@ -9,13 +10,82 @@ import os
 
 app = QApplication(sys.argv)
 window = QWidget()
-#FONT_SIZE = 9
+#FONT_SIZE : int = 9
+
+DEFAULT22_BASE64 : str = ""\
+"iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAAEdUlEQVR42qWUezBcZxjG96/e/vJv"\
+"pzONS9a6Lva+bgmxu0SVSTRIK0uqUdego6RoNSTRIDZWWJdlsYxWXYO0BLusIdaiLksGHcNUqtUy"\
+"homO1NOza5hRpKb5Zp6Zc77vnN/znuc985JIL1mCiqfhAtnUHy4i9bMzUbXRzNsjZ6k3ntiS/u9i"\
+"udwy4D0YUbnXzOOcbBaOYi1Yd0ZhkzQIs+sqWPrWvaB61azTBOWSE0Ntz+fFOCR3/aWDupXPwenB"\
+"FDh3f4JtihqmIZ0wDD4ockDrjgNXnHss0M79vgHVs/h7K2E9+JVz4BFyLpiCo2gC7Bt9sPBrOgTd"\
+"k5GwA1Y+tbNHgqmeJROm/nVwzJ0AX/4znCTTYOZNwD57HNSPmkAOfHQYerULRiHdML6mgPGnSth5"\
+"VI4dgDr4FMtN/ethFNQO5zwtaKJhmCd3EDFoYZ0wAHKYEsZX2kHxa4ZpQMs+zITYPx3ZC3JMHyjx"\
+"AzAOVYLjXJC3G4FAZGjuWw0ToqLotE64Fj2FX2QWrvKdwEgbBTmiR/+pZpeaQDQMdLcyWPs17MPM"\
+"k9SgpGpgkjgAw5AumArbsNssj/wMckADvOJ/xPr6OkLrJuHxRQPsUobgGqeAtH4KZcReurgf12If"\
+"4nJgFZyZ90CN7AD59ghMMkdhlKGBcZQSppFdMIvtxllXcQOJ6iWdPR3Yiiejv6CqfRCNs1r4lY4T"\
+"13PY2to6VkXqZzDMG8O7OSNE9QqYx3XDMqEb1GQFnLyKVkkWFyqJxrRhe3sbm5ubSG4cxe9/brwU"\
+"qtPwr+s4lTMEswQlrBK7YZOigN1NBRjp3XDyK98hmV+sIprWgS71oh6+sLCApaWl/wSvrKzAMkUJ"\
+"my8VoKUpwMxQgn1XAW5YPbj+cujBIfc0CM5X68E6jY+PHwnT9WBychL9/f3QarWgpxPAb4g/IYsA"\
+"ft0O7uctYAu/AzuwBkQUFeib+G0fuieNRrMPnJmZwdDQEKanp/Vx6c5XN56DI+oB99Zj2Kd3gB3e"\
+"CNYn9WB9XAd2cC1INF/ZUk7t1CHw8vIyent79dWvrq4eOpeqZnFRrAQjuhXMyBYww5vBDGsCK3TX"\
+"gGTjmS+Kyuzd+feLJ1HjwAIYcT+AEdO2axBFGEQ0gxNYs0Gi87INqb6V25XSbzGiGd3/1JPofIYK"\
+"jOtt+krpOoPYR8R9K+w/qKjenRP83FJLatJzF8oVfBWfieICGZQK1ZER7GlycQ3i2jE4CApBS3ys"\
+"N6DHt+tMXriwsw3254U17/68+dtCUN7yBPlNj9QzFpeHCgukbeVl8rUepervo0xuZnSCHVANrreM"\
+"iOChPg5HvuTDA4OIzssysHK+s0B5RwiTNwRBRq/zDE+9dk7vXFhYaCvJLzlgMji8CC6/CPZuEnAu"\
+"ycEKqoUDX1J97Fy2on5WYkaJUB13rjNJTS9ROXlLdzjuJeAKCDivkDAokJFeZTF8KnyY75evsd4r"\
+"W2N7loLjIZ3nuBf7vBKUdrE6lX5BnsrwqRQxvcsbmV6yoOOe/QfLaRAj5n2jwQAAAABJRU5ErkJg"\
+"gg=="
+
+KEY_SCREENSHOT_BASE64: str = ""\
+"iVBORw0KGgoAAAANSUhEUgAAADQAAAA1CAIAAACBRl8ZAAAAA3NCSVQICAjb4U/gAAAIiElEQVRo"\
+"Q+2YiVPb2B3HJVuWLdmy5Eu2uQ3EsYEESAgUsknJZrOZ5tgmk9lmttM/rdP+AZ1OZ7rtNtlt2E0I"\
+"R5zQJdw2PjDgC/k+5Nvan3Enm00NGAdmMp08ZrD09J7e533f77LRWDyBfKhN9KGCVbk+wjV7Oh+V"\
+"+79UDmt2V0fMKxaLsXgklUqmMslclheLMUqhJOVyBalQKlUY1tC66GnEOb9/d8O+ajafZRhGSTEK"\
+"uUIQhAyfBtZwJOzZ8ljO9hn0LUfsD0FOGK5QKCwuLSBI5crlX8ukRCDo8wf9xWI+ny+yOp2cVGg1"\
+"WpFIPD3zFBHQ4eERiQQ/BPEkvRUQpn74tr2t7frkjS2ve3V9eX5+ToQiXu+OVqtxOp2ReHR57fVe"\
+"OHT92uc9vb3fPPq6UMgfAneSyr2wzZq6ukxd3S9ezYlEIpfTdevWnVKhRDNMsVCoVATPllun0wYC"\
+"foVCYTL1+n2+xaXFa5OfHcR3Ysrt+naKxUJvj/nlwguSIMsF4auHf0gnM2D7CwuvCsVCOML1dPfK"\
+"ZGRv71kUEfv8252dXZUK4nI7TxcOfHNleXHy6qf+gE+K43w6Oz4x8Wz6GaWkVlZXB88P2e32jY3V"\
+"x999w2cyIhRlWb1QQaPx6N3bt+fmpsEe6vKdjHJceK+rq4sgiB2fl9uL/Wp8Yub5zNDg4NLrpaHB"\
+"obW1lXBk74y579y54Zm558lkAkEFWkmn4AJBLgyPwJZOES4RjxmNrdlsNpPir1z5xGF3WK2WxcXF"\
+"vj5rNBoJRzmLZYDV6fWsYXDwwszsjJyUc2EOx6U8zxsMBm5v7zThkjElpYxEOZPJVCqV1GrV0tJr"\
+"i+Wsy+ViWTaXy1fAuPZbpVxBUATDJCiKBPx+iH8gIccFTxMuESdJOView76JilAggwjs8bjb2lrT"\
+"6dRA/8CPP9rASf1+n802e/+3D4AvnUmpVExFqIAxhMNcXbiG0kjdmW93VgRBLBanM+kz5p5SsWQ2"\
+"nwGyjo72UCio1xu6Ok0ogtpe2UC20UtjOI5DrIb453Rujo1NVEUFGeu1k4FTKCgIpwzNuF0uECMa"\
+"iWm06lAoBLE3Ho9WKuXW1rbf954RBCSb5RcWXmKYKJFIgq5iDEsk4jCxHhtyMnCUguL5DIqKWlpb"\
+"Uskkq2dz+RytolOpFEHIksk4z6ehkSS5T6wNR6MKuTwcjra3d3HcHqs31IU7gVACwoBhlSsVKS4B"\
+"q5IRJKxEUZQUlzIqTTqdQRAhEoniuGR3dxfsbHtnGxUhDvuG1WqFUiAQDLLs6cABme3l7M0bv9Fp"\
+"2Vg8CufI8ymwMAQMSYxiErFarea4MEFId3Z21WrG690mSNK34xsauiCXK0ulssu12WKoX6G8l3KV"\
+"cnlmfrrF2ArlWjTK5XM8xBGw9zSfgBiGS6QgoUKhlMsJn8+nUjNcJKKgqVAwONDf391thm3M2+Yu"\
+"XhwD3BM+1irZ7LM2Y6vlrHXNsQyhAUJJuVTMV6OaENrzATFNMeDC2UKBYpSpdIammcBuYGx0vLfX"\
+"AgF7y7tVLJas1v66ZNDZpHLg/89nnra0GPus/Q73hoE15rI5oJHg0sHzF41QSKKI2+2Ees7jdqtp"\
+"VSwc72hrL+RKDx78TqtlS6Wid3vL9tJ2bfLGQWTQ30zJVCV7/r3RaDx/bsjp2dDrWgr5fCwWKQsC"\
+"LsPN3VaIF7s+LwxTMVqapsGLoTYpl+E5eAYH1dTs7FwmmwYy8JtD4I4dSmDJ6ekpg6FKtumx63VG"\
+"REAymTQqFkkxcWd7t8+3DRxQBkPwg81DxQE0uRwk3izkWQgcq+urkGEH+s8fglV7dDzlgOzZ9JSe"\
+"1UMp4fSsszqjRIxDGAMZIJwCGUgI8gANZHSIxl//4++ABS5SLpalhFSjZVkt29HRuc99dDuGcjUy"\
+"nU538cKIw73Oag2ElITQyuezElzS2dYNDsFx3F//9pfL458MD19MJpMqlfqz6zchWUFyg3Y0zi9H"\
+"NAoHZE+fTem0mtGRMbtrDciUCjqbzSXSMYkEAzKodQPBwL8e/fPevS/n56ahIhJjYgh+sBwod1ys"\
+"YxwrnNT3T59o1ZqJ8csbzhWdRk8p6AQkpWxaRasZWg3XUDBOTT354s59mlGVyyWPx53N8QP9g+gB"\
+"Sb0R3KNtrkr2w3dKmobDcm3ZgQwCbDDk43M8OGYg4HM4HNl8NhQM3b19D8gaWbXBMUccK5A9efIt"\
+"KSdHR0adHjscE6SmdccyhokhgshkxKNHj0cujZEk8enkzSas6nDKw+CA7N9PHmMSbHT0knfXDZpB"\
+"jFi2/0dJ0UZ9u1atA0OUETL4LggF4+HLNPf0QLiaZvBN6fL4BByiTsOC9cCxmnv6GKUal+B74aDH"\
+"40EE4ZTIYD8Hws2/eA4V/9Wrk0HOr1WzGIZvutflJAWu8Kc//xEkhMmsXn/z81vNqdLIrAPh1tZW"\
+"Hz58yEVCQAa2FdzzdbaZIJEThJxhVPfvfdnI299zzIFwUPxATtRodISM9O64FXJqw+FYWVqRSMQW"\
+"S997rtrg9APhYD4YPjjgjt8LEaurveel7dXdO19QFN3gr2sNEhwyrA4c+CB4gwKsS06l0glSRrYZ"\
+"O0C/TCoDlSM8gjRVe+M7AfZ9busi1gnCkArB3uE3BM+WE/J3bZoEw81mi8nUUwtmwFFDefP/rYvq"\
+"jGqlXv34edhB4+ti1TrrFJtCpZJMJKRSgtW1MkqtCMErZREuIeLxpNvthkwP0gI9NLh4u4Go+7fw"\
+"iez/g79fNFiy2r3f3lwcAldHOXCF/TXKUEODiqViERVVq0VoIBuUQ7XX1XLmfxWCrv37mjxvLhq8"\
+"PYivDtybbdU2984W36xX942HP32bvu70dzrrOMT/7ruRF53GmDo2dxrLNPfOj3DN6db099Zmlzve"\
+"vI/Hejy9fh79QSv3E643X0RfREFsAAAAAElFTkSuQmCC"
+
 
 window.setWindowTitle("Password Required - Mozilla Thunderbird")
 #window.setFont(QtGui.QFont("Arial", FONT_SIZE))
 # TODO: Include the default22.png Thunderbird icon from a system directory,
 # depending on the previously determined installation path of Thunderbird.
-window.setWindowIcon(QtGui.QIcon(os.path.join("data", "default22.png")))
+with open(os.path.join("data", "default22_decoded.png"), "wb") as tb_icon:
+    tb_icon.write(base64.b64decode(DEFAULT22_BASE64))
+window.setWindowIcon(QtGui.QIcon(os.path.join("data", "default22_decoded.png")))
 window.setGeometry(100, 100, 390, 150)
 
 # Let the window sporn in the center of the screen:
@@ -25,7 +95,9 @@ fg.moveCenter(center_point)
 window.move(fg.topLeft())
 
 key = QLabel(parent=window)
-pixmap = QtGui.QPixmap(os.path.join("data", "key_screenshot.png"))
+with open(os.path.join("data", "key_screenshot_decoded.png"), "wb") as key_icon:
+    key_icon.write(base64.b64decode(KEY_SCREENSHOT_BASE64))
+pixmap = QtGui.QPixmap(os.path.join("data", "key_screenshot_decoded.png"))
 key.setPixmap(pixmap)
 key.move(15,15)
 
