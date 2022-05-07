a2e55675bd562ee4eeeab4594b26882d6c77b616
telekobold <dev@telekobold.de>
2022-05-07 12:03:31 +0200

    Masterpassword window: Add thunderbird icon

--- python/thunderbird_masterpassword_window.py
+++ python/thunderbird_masterpassword_window.py
@@ -1,11 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from PyQt5 import QtGui
+import os
 
 app = QApplication(sys.argv)
 window = QWidget()
 
-# TODO: Include the default22.png Thunderbird icon from a system directory.
 window.setWindowTitle("Password Required - Mozilla Thunderbird")
+# TODO: Include the default22.png Thunderbird icon from a system directory,
+# depending on the previously determined installation path of Thunderbird.
+window.setWindowIcon(QtGui.QIcon(os.path.join("data", "default22.png")))
 window.setGeometry(100, 100, 390, 150)
 # TODO: Let the window sporn in the center of the screen.
 window.move(50, 50)
