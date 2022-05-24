935bd559d25ed0ee33762a7c6eadf07b41dac669
telekobold <dev@telekobold.de>
2022-05-24 14:22:36 +0200

    Change license notice

--- python/filewriter.py
+++ python/filewriter.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python
 
 """
-"MODIFIED BEER-WARE LICENSE" (Revision 42):
-telekobold <mail@telekobold.de> wrote this file. As long as you 
-retain this notice you can do whatever you want with this stuff within the scope
-of this note. If we meet some day, and you think this stuff is worth it, you can 
-buy me a beer in return. telekobold.
+(c) 2022 telekobold <www.telekobold.de>
 
 This software was written solely for the joy of exploring how things work
 and the intension of sharing accumulated experiences with others. It shall not
-be used to cause harm to anyone. Please refer to the hacker ethics
+be abused to cause harm to anyone. Please refer to the hacker ethics
 <https://www.ccc.de/en/hackerethics>, especially the point "Don't litter other 
 people's data."
 
